28a9563340272e8aa75c582a33520aa5b432b9cd
Бонаква <zefirax89@gmail.com>
2025-03-31 06:46:34 +0300

    Add files via upload

---
+++ Estimating the complexity of recursive algorithms2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите строку: ");
+        string ss = Console.ReadLine();
+
+        string cleane = ss.Replace(" ", "").ToLower();
+
+        bool pp = Palindrom(cleane);
+
+        if (pp)
+        {
+            Console.WriteLine("Строка является палиндромом");
+        }
+        else
+        {
+            Console.WriteLine("Строка не является палиндромом");
+        }
+    }
+
+    static bool Palindrom(string str)
+    {
+        int left = 0;
+        int right = str.Length - 1;
+
+        while (left < right)
+        {
+            if (str[left] != str[right])
+            {
+                return false;  
+            }
+            left++;
+            right--;
+        }
+
+        return true;  
+    }
+}
